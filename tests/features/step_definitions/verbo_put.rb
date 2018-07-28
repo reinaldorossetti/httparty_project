35e66217b6a0f74ab221177a92f93488c00399d3
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-07-28 14:12:25 -0300

    Update verbo_put.rb

--- tests/features/step_definitions/verbo_put.rb
+++ tests/features/step_definitions/verbo_put.rb
@@ -1,25 +1,30 @@
 Dado('que eu altere os parametros do meu cliente de um cliente específico') do
+  
+  # gera os dados usando o Faker
   @cpf_alterado = Faker::CPF.numeric
   @endereco_alterado = Faker::StarWars.planet
   @usuario_alterado = Faker::Pokemon.name
   @senha_alterada = Faker::Base.numerify('inicial####')
-
+  
+  # adiciona os dados no json. 
   body_put = {
       cpf: @cpf_alterado,
       endereco: @endereco_alterado,
       usuario: @usuario_alterado,
       senha: @senha_alterada
   }.to_json
-
+  # pego um cliente aleatorio para ser alterado os dados.
   number = $GlobalPage.number
   get_cliente = Http.get "/#{number}"
   p "Cliente que vai ser alterado: #{get_cliente.body}"
   p "Alterar os dados para: #{body_put}"
+  # envia um post com os dados.
   Http.put "/#{number}", body: body_put
   @cliente = $GlobalPage.client_data(number)
 end
 
 Entao('esse cliente devera ser atualizado com sucesso') do
+  # fazendo todos os meus asserts.
   expect(@cliente['cpf']).to eq @cpf_alterado
   expect(@cliente['endereco']).to eq @endereco_alterado
   expect(@cliente['usuario']).to eq @usuario_alterado
